749c9b003f367a282ff93260b177f56cb53292a9
Диана <dianakondrashova7@gmail.com>
2023-10-25 15:20:15 +0300

    функции начало

---
+++ js/functions.js
@@ -0,0 +1,29 @@
+function checkStrLenght(string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkPalindrom(string) {
+  let normString = string.replaceAll(' ', '');
+  normString = normString.toUpperCase();
+
+  let newString = '';
+
+  for (let i = normString.length - 1; i >= 0; i--) {
+    const symbol = normString.at(i);
+    newString += symbol;
+  }
+
+  if (newString === normString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Test
+// console.log(checkStrLenght('hdhxhxh', 10));
+// console.log(checkPalindrom('di ana'));
